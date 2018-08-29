f2907e08e2a6f616fad77cda1bb50a31783d8c4a
jack <steve.a.shogren@gmail.com>
2018-08-29 05:54:06 -0400

    passing results to tbl, but versions seem to be different

--- databases.sh
+++ databases.sh
@@ -12,4 +12,6 @@ awk -f countriesqueries/prep1.awk countries | awk -f countriesqueries/form2.awk
 
 awk -f countriesqueries/prep3.awk pass=1 countries pass=2 countries | awk -f countriesqueries/form3.awk
 
+awk -f countriesqueries/prep3.awk pass=1 countries pass=2 countries | awk -f countriesqueries/form4.awk | tbl -Tascii
+
 set +v
